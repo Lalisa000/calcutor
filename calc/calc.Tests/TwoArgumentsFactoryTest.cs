b44d4e8cd4d7f80846fc83a40ffc3b6310be000f
Lalisa000 <anna.zaharets@gmail.com>
2019-07-11 13:49:49 +0600

    Create TwoArgumentsFactory Test

---
+++ calc/calc.Tests/TwoArgumentsFactoryTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc.Tests
+{
+    [TestFixture]
+    public class TwoArgumentFactoryTestCase
+    {
+        [TestCase("summary", typeof(Additioner))]
+        [TestCase("arithmMean", typeof(ArithmeticMean))]
+        [TestCase("Degree", typeof(Degree))]
+        [TestCase("division", typeof(Divisioner))]
+        [TestCase("geomMean", typeof(GeometricMean))]
+        [TestCase("LogXY", typeof(LogXY))]
+        [TestCase("multiplication", typeof(Multiplicator))]
+        [TestCase("subtraction", typeof(Subtrackter))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
