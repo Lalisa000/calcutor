6641f2062e3d0d2b4d60e477795903ea750d62c1
Lalisa000 <anna.zaharets@gmail.com>
2019-07-11 13:41:20 +0600

    Create OneArgumentsFactory Test

---
+++ calc/calc.Tests/ObjectOrientedCalculatorTestsFactories.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc.Tests
+{
+    [TestFixture]
+    public class OneArgumentFactoryTestCase
+    {
+        [TestCase("abs", typeof(Abs))]
+        [TestCase("acos", typeof(Arccos))]
+        [TestCase("arcctg", typeof(Arcctan))]
+        [TestCase("asin", typeof(Arcsin))]
+        [TestCase("arctg", typeof(Arctan))]
+        [TestCase("cos", typeof(Cos))]
+        [TestCase("ctan", typeof(Ctan))]
+        [TestCase("Degree10", typeof(Degree10))]
+        [TestCase("exp", typeof(Exp))]
+        [TestCase("exponentiating", typeof(Exponentor))]
+        [TestCase("extraction", typeof(Extractor))]
+        [TestCase("logtwo", typeof(LogTwo))]
+        [TestCase("ln", typeof(NaturalLog))]
+        [TestCase("Negative", typeof(Negative))]
+        [TestCase("reverse", typeof(Reverse))]
+        [TestCase("sin", typeof(Sin))]
+        [TestCase("tan", typeof(Tan))]
+        [TestCase("two", typeof(Two))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
